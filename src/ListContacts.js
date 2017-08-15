d58ab9450affe848974cf722fc1d931d6c3f791b
nehap12 <neha.paul@sjsu.edu>
2017-08-15 15:14:26 -0700

    Adding files to contacts application

---
+++ src/ListContacts.js
@@ -0,0 +1,121 @@
+/**
+ * Created by NehaP on 8/2/2017.
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class ListContacts extends Component {
+
+    static propTypes = {
+    contacts: PropTypes.array.isRequired ,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+      query: ''
+  }
+
+
+  updateQuery = (query) => {
+    let trimmedQuery = query.replace(/^\s+|\s+$/g, '')
+    this.setState({
+      query: trimmedQuery
+    })
+  }
+
+
+  clearQuery = () => {
+      this.setState({ query:'' })
+  }
+
+
+
+render() {
+    const { contacts, onDeleteContact } = this.props
+    const { query } = this.state
+
+    let showingContacts
+
+    if(query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
+    } else {
+      showingContacts = contacts
+    }
+
+    showingContacts.sort(sortBy('name'))
+
+  return (
+    <div className="list-contacts">
+      <div className="list-contacts-top">
+        <input
+          className="search-contacts"
+          type="text"
+          placeholder="Search Contacts"
+          value={query}
+          onChange={(event) => this.updateQuery(event.target.value)}
+        />
+      </div>
+
+      {showingContacts.length !== contacts.length && (
+        <div className="showing-contacts">
+          <span>Now Showing {showingContacts.length} of {contacts.length} total</span>
+          <button onClick={this.clearQuery}>Show All</button>
+        </div>
+      )}
+
+      <ol className='contact-list'>
+        {showingContacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+              <div className="contact-avatar" style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}/>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button onClick={() => onDeleteContact(contact)} className="contact-remove">
+                Remove
+              </button>
+            </li>
+          )
+        )}
+      </ol>
+    </div>
+  )
+}
+}
+
+
+
+
+/* Instead of the class written below we will use the Stateless Functional Components, we don't use "this" in this funnction
+-----------------------------------------------------------------------------------------------------------------------------
+class ListContacts extends Component {
+  render() {
+    return (
+      <ol className='contact-list'>
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
+            <div className="contact-avatar" style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className="contact-remove">
+              Remove
+            </button>
+          </li>
+          )
+        )}
+      </ol>
+    )
+  }
+}*/
+
+export default ListContacts
